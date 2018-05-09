29e9eea985abaacc3942bb184ec2a320d415eda0
Yegor Smekhov <egor.smekhov@gmail.com>
2018-05-09 15:53:35 +0300

    added types

---
+++ src/app/types/character.ts
@@ -0,0 +1,28 @@
+export class CharacterProperty {
+
+    constructor(name, value) {}
+    name: string;
+    value: any;
+}
+
+export class Character {
+
+    constructor() {
+        this.properties = [];
+    }
+
+    name: string;
+    properties: CharacterProperty[];
+
+    static create(obj: any) {
+        const char = new Character();
+        for (const propName of Object.keys(obj)) {
+            if (propName === 'name') {
+                char.name = obj[propName];
+            } else {
+                char.properties.push(new CharacterProperty(propName, obj[propName]));
+            }
+        }
+        return char;
+    }
+}
---
+++ src/app/types/team.ts
@@ -0,0 +1,6 @@
+import { Character } from './character';
+
+export default class Team {
+    name: string;
+    characters: Character[];
+}
