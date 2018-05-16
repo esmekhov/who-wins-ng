233ee7f7107b837dbb75945c2b74771b28be909b
Yegor Smekhov <egor.smekhov@gmail.com>
2018-05-16 22:54:25 +0300

    added navigation for editor

--- src/app/pages/team-editor-page/team-editor-page.component.ts
+++ src/app/pages/team-editor-page/team-editor-page.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 import Team from '../../types/team';
 import { SimulationService } from '../../services/simulation.service';
+import { State, getLayout, getSimulationOptions, getTeams } from '../../reducers';
+import { getTeam } from '../../reducers/teams';
 
 @Component({
   selector: 'app-team-editor-page',
@@ -10,11 +17,14 @@ import { SimulationService } from '../../services/simulation.service';
 export class TeamEditorPageComponent implements OnInit {
 
   private team: Team;
+  private team$: Observable<Team>;
 
-  constructor(private simulationService: SimulationService) { }
+  constructor(private route: ActivatedRoute, private store: Store<State>) { }
 
   ngOnInit() {
-    this.simulationService.getTeams().filter(t => t.name === name);
+    this.team$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.store.pipe(select(getTeams), select(getTeam(params.get('name')))))
+    );
   }
 
 }
